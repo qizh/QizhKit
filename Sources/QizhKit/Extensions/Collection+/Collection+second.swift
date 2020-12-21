8f771c9d834084f49a590652e47897431cb76306
Serhii <zh.send@gmail.com>
2020-12-21 14:29:59 +0700

    Collection penultimate element and index

--- Sources/QizhKit/Extensions/Collection+/Collection+second.swift
+++ Sources/QizhKit/Extensions/Collection+/Collection+second.swift
@@ -12,8 +12,10 @@ public extension Collection {
 	@inlinable var secondIndex: Index { index(after:  startIndex) }
 	@inlinable var  thirdIndex: Index { index(after: secondIndex) }
 	@inlinable var fourthIndex: Index { index(after:  thirdIndex) }
+	@inlinable var penultimateIndex: Index { index(endIndex, offsetBy: -1) }
 
 	@inlinable var second: Element? { self[safe: secondIndex] }
 	@inlinable var  third: Element? { self[safe:  thirdIndex] }
 	@inlinable var fourth: Element? { self[safe: fourthIndex] }
+	@inlinable var penultimate: Element? { self[safe: penultimateIndex] }
 }
