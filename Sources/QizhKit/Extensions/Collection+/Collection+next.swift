f494b89e669ca4b5ed6cd6fbd9779f09fbf343f8
Serhii <zh.send@gmail.com>
2021-02-05 20:47:37 +0700

    Collection previous element

--- Sources/QizhKit/Extensions/Collection+/Collection+next.swift
+++ Sources/QizhKit/Extensions/Collection+/Collection+next.swift
@@ -9,10 +9,21 @@
 import Foundation
 
 public extension Collection {
-	@inlinable func next(after element: Element) -> Element? where Element: Equatable {
+	func next(after element: Element) -> Element? where Element: Equatable {
 		if let currentIndex = firstIndex(of: element),
-		   let nextIndex = index(currentIndex, offsetBy: .one, limitedBy: index(endIndex, offsetBy: .minusOne)) {
+		   let nextIndex = index(currentIndex, offsetBy: .one, limitedBy: index(endIndex, offsetBy: .minusOne))
+		{
 			return self[nextIndex]
+		}
+		return .none
+	}
+	
+	func previous(before element: Element) -> Element? where Element: Equatable {
+		if let currentIndex = firstIndex(of: element),
+		   currentIndex != startIndex
+		{
+			let prevIndex = index(currentIndex, offsetBy: .minusOne)
+			return self[prevIndex]
 		}
 		return .none
 	}
