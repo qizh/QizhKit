d74e1b9efd585ab09f59a0c8c8ae41bba27d0cdd
Serhii <zh.send@gmail.com>
2021-03-10 16:28:15 +0700

    locale.localizedCurrencySymbol(forCurrencyCode: isoCode)

--- Sources/QizhKit/Extensions/Locale+/Locale+24h.swift
+++ Sources/QizhKit/Extensions/Locale+/Locale+24h.swift
@@ -19,3 +19,27 @@ public extension Locale {
 		!isUsing12HFormat
 	}
 }
+
+public extension Locale {
+	func localizedCurrencySymbol(forCurrencyCode currencyCode: String) -> String? {
+		guard let languageCode = languageCode, let regionCode = regionCode else { return nil }
+
+		/*
+		 Each currency can have a symbol ($, £, ¥),
+		 but those symbols may be shared with other currencies.
+		 For example, in Canadian and American locales,
+		 the $ symbol on its own implicitly represents CAD and USD, respectively.
+		 Including the language and region here ensures that
+		 USD is represented as $ in America and US$ in Canada.
+		*/
+		let components: [String: String] = [
+			NSLocale.Key.languageCode.rawValue: languageCode,
+			NSLocale.Key.countryCode.rawValue: regionCode,
+			NSLocale.Key.currencyCode.rawValue: currencyCode,
+		]
+
+		let identifier = Locale.identifier(fromComponents: components)
+
+		return Locale(identifier: identifier).currencySymbol
+	}
+}
