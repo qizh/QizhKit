6e0cda87eff25e461c95ae9717de3652fb7368eb
Serhii <zh.send@gmail.com>
2021-03-16 23:00:34 +0700

    Encodable to Dictionary, CodableAnyDictionary updates

--- Sources/QizhKit/Structures/Codable/CodableAnyDictionary.swift
+++ Sources/QizhKit/Structures/Codable/CodableAnyDictionary.swift
@@ -8,20 +8,60 @@
 
 import Foundation
 
-@propertyWrapper public struct CodableAnyDictionary: Codable {
+@propertyWrapper
+public struct CodableAnyDictionary: ExpressibleByDictionaryLiteral, EmptyProvidable {
 	public var wrappedValue: [String: Any]
 	
-	public init(wrappedValue: [String: Any]) {
+	public init(wrappedValue: [String: Any] = .empty) {
 		self.wrappedValue = wrappedValue
 	}
 	
+	public init(dictionaryLiteral elements: (String, Any)...) {
+		self.wrappedValue = .init(uniqueKeysWithValues: elements)
+	}
+	
+	@inlinable
+	public static func some(_ value: [String: Any]) -> Self {
+		.init(wrappedValue: value)
+	}
+	
+	public static let empty: Self = .init()
+}
+
+// MARK: Codable
+
+extension CodableAnyDictionary: Codable {
 	public init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: JSONCodingKeys.self)
 		wrappedValue = try container.decode([String: Any].self)
 	}
 	
 	public func encode(to encoder: Encoder) throws {
+		guard wrappedValue.isNotEmpty else { return }
 		var container = encoder.container(keyedBy: JSONCodingKeys.self)
 		try container.encode(wrappedValue)
+	}
+}
+
+public extension KeyedDecodingContainer {
+	func decode(_: CodableAnyDictionary.Type, forKey key: Key) -> CodableAnyDictionary {
+		(try? decodeIfPresent(CodableAnyDictionary.self, forKey: key))
+			?? .empty
+	}
+}
+
+// MARK: Encodable -> Dictionary
+
+public extension Encodable {
+	func asDictionary() -> [String: Any] {
+		do {
+			let encoder = JSONEncoder()
+			let data = try encoder.encode(self)
+			let decoder = JSONDecoder()
+			let dictionary = try decoder.decode(CodableAnyDictionary.self, from: data)
+			return dictionary.wrappedValue
+		} catch {
+			return .empty
+		}
 	}
 }
