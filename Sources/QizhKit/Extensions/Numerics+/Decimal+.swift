7c923dd09d281af603d5a137f9f82a4ff3a8b2fc
Serhii <zh.send@gmail.com>
2021-03-24 17:58:25 +0700

    Decimal round

--- Sources/QizhKit/Extensions/Numerics+/Decimal+.swift
+++ Sources/QizhKit/Extensions/Numerics+/Decimal+.swift
@@ -61,3 +61,19 @@ public extension Decimal {
 		case percent
 	}
 }
+
+// MARK: Round
+
+public extension Decimal {
+	mutating func round(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) {
+		var localCopy = self
+		NSDecimalRound(&self, &localCopy, scale, roundingMode)
+	}
+
+	func rounded(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
+		var result = Decimal()
+		var localCopy = self
+		NSDecimalRound(&result, &localCopy, scale, roundingMode)
+		return result
+	}
+}
