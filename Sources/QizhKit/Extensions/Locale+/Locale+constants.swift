5f6e9a83bc3de050726e03c3cfdad9cc4e284bd1
Serhii <zh.send@gmail.com>
2021-03-31 17:09:04 +0700

    Locale constants

---
+++ Sources/QizhKit/Extensions/Locale+/Locale+constants.swift
@@ -0,0 +1,15 @@
+//
+//  Locale+constants.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 31.03.2021.
+//  Copyright © 2021 Serhii Shevchenko. All rights reserved.
+//
+
+import Foundation
+
+public extension Locale {
+	static var en_US: Locale { .init(identifier: "en_US") }
+	static var ru_UA: Locale { .init(identifier: "ru_UA") }
+	static var th_TH: Locale { .init(identifier: "th_TH") }
+}
