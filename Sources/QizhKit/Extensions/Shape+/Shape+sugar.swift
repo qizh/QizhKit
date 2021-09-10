ac863ad3b4f272d32c4d739afb977b38953b1237
Serhii Shevchenko <zh.send@gmail.com>
2021-09-10 21:56:09 +0700

    Shape fill + stroke

--- Sources/QizhKit/Extensions/Shape+/Shape+sugar.swift
+++ Sources/QizhKit/Extensions/Shape+/Shape+sugar.swift
@@ -13,3 +13,27 @@ extension View {
 		clipShape(Circle())
 	}
 }
+
+extension Shape {
+	public func fill <Fill: ShapeStyle, Stroke: ShapeStyle> (
+		_ fillStyle: Fill,
+		strokeBorder strokeStyle: Stroke,
+		lineWidth: CGFloat = .one
+	) -> some View {
+		self
+			.stroke(strokeStyle, lineWidth: lineWidth)
+			.background(self.fill(fillStyle))
+	}
+}
+
+extension InsettableShape {
+	public func fill <Fill: ShapeStyle, Stroke: ShapeStyle> (
+		_ fillStyle: Fill,
+		strokeBorder strokeStyle: Stroke,
+		lineWidth: CGFloat = .one
+	) -> some View {
+		self
+			.strokeBorder(strokeStyle, lineWidth: lineWidth)
+			.background(self.fill(fillStyle))
+	}
+}
