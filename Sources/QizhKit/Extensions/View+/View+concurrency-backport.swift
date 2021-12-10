a4930ca4207a1d662c08aacafdb26901310bcbfe
Serhii Shevchenko <zh.send@gmail.com>
2021-12-10 14:15:43 +0700

    View's task modifier for iOS lower than 15

---
+++ Sources/QizhKit/Extensions/View+/View+concurrency-backport.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 10.12.2021.
+//  Copyright © 2021 Serhii Shevchenko. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS, obsoleted: 15, message: "Implemented in SwiftUI")
+extension View {
+	@inlinable
+	public func task(
+		priority: TaskPriority = .userInitiated,
+		_ action: @escaping @Sendable () async -> Void
+	) -> some View {
+		self.onAppear {
+			Task(priority: priority, operation: action)
+		}
+	}
+	
+	@available(iOS 14.0, *)
+	@inlinable
+	public func task<T>(
+		id value: T,
+		priority: TaskPriority = .userInitiated,
+		_ action: @escaping @Sendable () async -> Void
+	) -> some View where T : Equatable {
+		self.onChange(of: value) {
+			Task(priority: priority, operation: action)
+		}
+	}
+}
