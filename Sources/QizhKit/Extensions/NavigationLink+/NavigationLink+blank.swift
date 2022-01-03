394d7e55eda29ab4705be791a9ec51ba22cbb45c
Serhii Shevchenko <zh.send@gmail.com>
2022-01-03 16:00:56 +0700

    Remove NavigationLink's "Label == Text" convenient initializers since they are implemented in iOS 15 and back ported

--- Sources/QizhKit/Extensions/NavigationLink+/NavigationLink+blank.swift
+++ Sources/QizhKit/Extensions/NavigationLink+/NavigationLink+blank.swift
@@ -27,28 +27,3 @@ public extension NavigationLink where Label == Pixel {
 		}
 	}
 }
-
-public extension NavigationLink where Label == Text {
-	@inlinable init<S>(_ title: S, destination: Destination)
-		where S: StringProtocol
-	{
-		self.init(destination: destination, label: { Text(title) })
-	}
-	
-	@inlinable init<S>(_ title: S, destination: Destination, isActive: Binding<Bool>)
-		where S: StringProtocol
-	{
-		self.init(destination: destination, isActive: isActive, label: { Text(title) })
-	}
-}
-
-/*
-public extension NavigationLink where Label == Pixel {
-	@inlinable static func Pixel(
-		destination: Destination,
-		isActive: Binding<Bool>
-	) -> NavigationLink {
-		NavigationLink(destination: destination, isActive: isActive, label: Label.init)
-	}
-}
-*/
