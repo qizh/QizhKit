99b5230b1e4ac890cccf0df3129c0914bdc1e5cb
Serhii Shevchenko <zh.send@gmail.com>
2022-01-31 17:06:57 +0700

    Type erasing shapes: AnyShape and AnyInsettableShape

---
+++ Sources/QizhKit/Structures/Type Erase/AnyShape.swift
@@ -0,0 +1,52 @@
+//
+//  AnyShape.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 31.01.2022.
+//  Copyright © 2022 Serhii Shevchenko. All rights reserved.
+//
+
+import SwiftUI
+
+public struct AnyShape: Shape {
+	private let makePath: (CGRect) -> Path
+	
+	public init<S: Shape>(_ shape: S) {
+		makePath = shape.path(in:)
+	}
+	
+	public func path(in rect: CGRect) -> Path {
+		makePath(rect)
+	}
+}
+
+public struct AnyInsettableShape: InsettableShape {
+	private let makePath: (CGRect) -> Path
+	private let makeInset: (CGFloat) -> AnyInsettableShape
+	
+	public init<S: InsettableShape>(_ shape: S) {
+		makePath = shape.path(in:)
+		makeInset = { AnyInsettableShape(shape.inset(by: $0)) }
+	}
+	
+	public func path(in rect: CGRect) -> Path {
+		makePath(rect)
+	}
+	
+	public func inset(by amount: CGFloat) -> AnyInsettableShape {
+		makeInset(amount)
+	}
+}
+
+extension Shape {
+	@inlinable
+	public func asAnyShape() -> AnyShape {
+		AnyShape(self)
+	}
+}
+
+extension InsettableShape {
+	public func asAnyInsettableShape() -> AnyInsettableShape {
+		AnyInsettableShape(self)
+	}
+}
