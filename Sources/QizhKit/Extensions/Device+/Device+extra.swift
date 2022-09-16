3c15f01672f8ab2d84cece7ad9b158fe59148c34
Serhii Shevchenko <zh.send@gmail.com>
2022-09-16 18:34:32 +0700

    DeviceKit extension for dynamic island

---
+++ Sources/QizhKit/Extensions/Device+/Device+extra.swift
@@ -0,0 +1,24 @@
+//
+//  Device+extra.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 16.09.22.
+//  Copyright © 2022 Serhii Shevchenko. All rights reserved.
+//
+
+import Foundation
+import DeviceKit
+
+extension Device {
+	public static var allDevicesWithDynamicIsland: [Device] {
+		[
+			.iPhone14Pro,
+			.iPhone14ProMax,
+		]
+	}
+	
+	public var hasDynamicIsland: Bool {
+			isOneOf(Device.allDevicesWithDynamicIsland)
+		|| 	isOneOf(Device.allDevicesWithDynamicIsland.map(Device.simulator))
+	}
+}
