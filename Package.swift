df2ff5e929f71ee5fcb33f733da6868c7b040bd7
Serhii Shevchenko <zh.send@gmail.com>
2023-03-30 13:48:22 +0700

    Switch back from customized `Introspect` dependency to the original one since it's been updated now

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 		),
     ],
 	dependencies: [
-		.package(url: "https://github.com/qizh/SwiftUI-Introspect", from: "0.1.6"),
+		.package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.2.3"),
 		.package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.4"),
 		.package(url: "https://github.com/devicekit/DeviceKit", from: "4.9.0"),
 		.package(url: "https://github.com/stleamist/BetterSafariView", from: "2.4.1"),
