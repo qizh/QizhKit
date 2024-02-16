d3ec35df1faa1976dcb6357fc67a94d566d3143d
Serhii Shevchenko <zh.send@gmail.com>
2024-02-16 17:23:22 +0700

    Interpolate by `DynamicTypeSize`

---
+++ Sources/QizhKit/Extensions/DynamicType+/DynamicTypeSize+interpolate.swift
@@ -0,0 +1,88 @@
+//
+//  DynamicTypeSize+interpolate.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 16.02.2024.
+//  Copyright © 2024 Serhii Shevchenko. All rights reserved.
+//
+
+import SwiftUI
+
+extension DynamicTypeSize {
+	public static let allRegularCases: [DynamicTypeSize] = allCases(in: .regularRange)
+	
+	@inlinable public static func allCases(
+		from start: DynamicTypeSize,
+		to end: DynamicTypeSize
+	) -> [DynamicTypeSize] {
+		allCases(in: start ... end)
+	}
+	
+	@inlinable public static func allCases(
+		in range: ClosedRange<DynamicTypeSize>
+	) -> [DynamicTypeSize] {
+		allCases.filter(range.contains(_:))
+	}
+}
+
+extension RangeExpression where Self == ClosedRange<DynamicTypeSize> {
+	public static var regularRange: ClosedRange<DynamicTypeSize> {
+		.xSmall ... .xxxLarge
+	}
+	
+	public static var fullRange: ClosedRange<DynamicTypeSize> {
+		.xSmall ... .accessibility5
+	}
+}
+
+extension RangeExpression where Bound: CaseIterable {
+	@inlinable public var cases: [Bound] {
+		Bound.allCases.filter(self.contains(_:))
+	}
+}
+
+extension ClosedRange where Bound: BinaryFloatingPoint {
+	public func interpolated(at progress: Bound) -> Bound {
+		lowerBound + (upperBound - lowerBound) * progress
+	}
+	
+	@inlinable public func interpolated<R>(
+		for size: DynamicTypeSize,
+		from sizes: R = .regularRange
+	) -> Bound
+		where R: RangeExpression,
+			  R.Bound == DynamicTypeSize
+	{
+		self.interpolated(at: size.progress(in: sizes))
+	}
+}
+
+extension DynamicTypeSize {
+	public func interpolate<R, F>(
+		_ range: ClosedRange<F>,
+		from types: R
+	) -> F
+		where R: RangeExpression,
+			  R.Bound == DynamicTypeSize,
+			  F: BinaryFloatingPoint
+	{
+		range.interpolated(at: self.progress(in: types))
+	}
+	
+	public func progress<R, F>(in range: R = .regularRange) -> F where R: RangeExpression, R.Bound == DynamicTypeSize, F: BinaryFloatingPoint {
+		let cases = range.contains(self) ? range.cases : DynamicTypeSize.allCases
+		let currentIndex = cases.firstIndex(of: self).forceUnwrapBecauseTested()
+		return F(currentIndex) / F(cases.count)
+	}
+	
+	/*
+	public static func progress<R, F>(
+		for target: DynamicTypeSize,
+		in range: R = .regularRange
+	) -> F where R: RangeExpression, R.Bound == DynamicTypeSize, F: BinaryFloatingPoint {
+		let cases = range.contains(target) ? range.cases : DynamicTypeSize.allCases
+		let currentIndex = cases.firstIndex(of: target).forceUnwrapBecauseTested()
+		return F(currentIndex) / F(cases.count)
+	}
+	*/
+}
