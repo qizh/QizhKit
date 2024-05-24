1df5aa6284b92310a0838fee4d7e643c3cd9c7f9
Serhii Shevchenko <zh.send@gmail.com>
2024-05-24 17:36:32 +0700

    Adopt `RawRepresentable` for `Date`

---
+++ Sources/QizhKit/Extensions/Date+/Date+RawRepresentable.swift
@@ -0,0 +1,23 @@
+//
+//  Date+RawRepresentable.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 28.01.2023.
+//  Copyright © 2024 Serhii Shevchenko. All rights reserved.
+//
+
+import Foundation
+
+extension Date: RawRepresentable {
+	public init?(rawValue: String) {
+		if let value = Double(rawValue) {
+			self.init(timeIntervalSinceReferenceDate: value)
+		} else {
+			return nil
+		}
+	}
+	
+	public var rawValue: String {
+		timeIntervalSinceReferenceDate.s0
+	}
+}
