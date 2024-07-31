17ff44173536a10afaa2542158f88b8f67288810
Serhii Shevchenko <zh.send@gmail.com>
2024-07-31 19:58:37 +0700

    Improve `Text` extension: - Add functions providing amounts for `spaceText`, `nonBreakingSpaceText`, `newLineText` - Multiply `Text` by `UInt`

--- Sources/QizhKit/Extensions/Text+/Text+empty.swift
+++ Sources/QizhKit/Extensions/Text+/Text+empty.swift
@@ -10,7 +10,26 @@ import SwiftUI
 
 extension Text {
 	public static let emptyText = Text(String.empty)
+	
 	public static let spaceText = Text(String.space)
+	public static func spaceText(_ count: UInt) -> Text {
+		Text(String.space * count)
+	}
+	
 	public static let nonBreakingSpaceText = Text(String.nonBreakingSpace)
+	public static func nonBreakingSpaceText(_ count: UInt) -> Text {
+		Text(String.nonBreakingSpace * count)
+	}
+	
 	public static let newLineText = Text(String.newLine)
+	public static func newLineText(_ count: UInt) -> Text {
+		Text(String.newLine * count)
+	}
+}
+
+extension Text {
+	public static func * (_ text: Text, _ count: UInt) -> Text {
+		Array<Text>(repeating: text, count: Int(count))
+			.joined(separator: .emptyText)
+	}
 }
