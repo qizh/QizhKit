4e9d2b630d4897f2f5cb9175ca460408492c17de
Serhii Shevchenko <zh.send@gmail.com>
2024-09-04 15:42:20 +0700

    Remove old and deprecated global `isRunningInPreview(_:)` func Use global `isInPreviewEnvironment` variable instead

--- Sources/QizhKit/Structures/Debug/InPreview.swift
+++ Sources/QizhKit/Structures/Debug/InPreview.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 
+/*
 /// Call it with no parameters to find out if the place you called it is running in a preview
 /// - Parameter function: A `#function` value
 @available(*, deprecated, renamed: "isInPreviewEnvironment", message: "Use the new more reliable global variable `isInPreviewEnvironment`")
 public func isRunningInPreview(_ function: String = #function) -> Bool {
 	function.hasPrefix("__preview__")
 }
+*/
 
 /// Checks the `ProcessInfo`'s environment for `XCODE_RUNNING_FOR_PREVIEWS` value
 /// and compares it with `1`
