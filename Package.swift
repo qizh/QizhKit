e8e7220109287198e4e6e045d9f63a5842f1a482
Serhii Shevchenko <zh.send@gmail.com>
2024-10-21 14:15:18 +0700

    Add `PrivacyInfo.xcprivacy`

--- Package.swift
+++ Package.swift
@@ -11,6 +11,8 @@ let package = Package(
     name: "QizhKit",
 	platforms: [
 		.iOS(.v16),
+		.macOS(.v13),
+		.macCatalyst(.v13),
 		.visionOS(.v1),
 	],
     products: [
@@ -20,30 +22,51 @@ let package = Package(
 		),
     ],
 	dependencies: [
+		/// Introspect
 		.package(url: "https://github.com/siteline/swiftui-introspect", from: "1.3.0"),
+		
+		/// Networking
 		.package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.1"),
+		
+		/// Device parameters
 		.package(url: "https://github.com/devicekit/DeviceKit", from: "5.5.0"),
 		// .package(url: "https://github.com/qizh/DeviceKit", from: "5.2.3"),
+		
+		/// In-app Safari
+		.package(url: "https://github.com/qizh/BetterSafariView", from: "2.4.3"),
 		// .package(url: "https://github.com/stleamist/BetterSafariView", from: "2.4.2"),
 		// .package(url: "https://github.com/shantanubala/BetterSafariView", branch: "main"),
-		.package(url: "https://github.com/qizh/BetterSafariView", from: "2.4.3"),
+		
+		/// Apple's
 		.package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
 		
 		/// Macros
-	    .package(url: "https://github.com/qizh/QizhMacroKit", from: "1.0.3"),
+	    .package(url: "https://github.com/qizh/QizhMacroKit", from: "1.0.5"),
 	],
     targets: [
         .target(
             name: "QizhKit",
 			dependencies: [
+				/// Introspect
 				.product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+				
+				/// Networking
 				"Alamofire",
+				
+				/// Device parameters
 				"DeviceKit",
+				
+				/// In-app Safari
 				"BetterSafariView",
+				
+				/// Apple's
 				.product(name: "OrderedCollections", package: "swift-collections"),
 				
 				/// Macros
 				.product(name: "QizhMacroKitClient", package: "QizhMacroKit"),
+			],
+			resources: [
+				.process("PrivacyInfo.xcprivacy")
 			]
 		),
     ]
