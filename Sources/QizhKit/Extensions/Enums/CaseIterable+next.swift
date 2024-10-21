5777c258ef36a5f6be4712d351263dedb5203384
Serhii Shevchenko <zh.send@gmail.com>
2024-10-22 00:04:34 +0700

    CaseIterable+next

---
+++ Sources/QizhKit/Extensions/Enums/CaseIterable+next.swift
@@ -0,0 +1,18 @@
+//
+//  CaseIterable+next.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 21.10.2024.
+//
+
+import Foundation
+
+extension CaseIterable {
+	public func next() -> Self? {
+		if let currentIndex = Self.allCases.firstIndex(of: self) {
+			Self.allCases[safe: Self.allCases.index(after: currentIndex)]
+		} else {
+			Self.allCases.first
+		}
+	}
+}
