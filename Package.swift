d37fcf7bdd6860b3bf26dd4c4613737a62f462dc
Serhii Shevchenko <zh.send@gmail.com>
2024-10-28 18:27:01 +0700

    Remove `QizhMacroKit` dependency

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
 		
 		/// Macros
-	    .package(url: "https://github.com/qizh/QizhMacroKit", from: "1.0.5"),
+	    // .package(url: "https://github.com/qizh/QizhMacroKit", from: "1.0.7"),
 	],
     targets: [
         .target(
@@ -63,7 +63,8 @@ let package = Package(
 				.product(name: "OrderedCollections", package: "swift-collections"),
 				
 				/// Macros
-				.product(name: "QizhMacroKitClient", package: "QizhMacroKit"),
+				// .product(name: "QizhMacroKit", package: "QizhMacroKit"),
+				// .product(name: "QizhMacroKitClient", package: "QizhMacroKit"),
 			],
 			resources: [
 				.process("PrivacyInfo.xcprivacy")
