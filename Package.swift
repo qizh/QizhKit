3b6666a8ed7ea47ea1c13f62ef7c5a1b542e4356
Serhii Shevchenko <zh.send@gmail.com>
2024-12-16 15:56:36 +0700

    Update `QizhMacroKit` to `1.0.14` supporting Swift `6.0.2` and higher up until but not including `6.1`

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
 		
 		/// Macros
-		.package(url: "https://github.com/qizh/QizhMacroKit", exact: "1.0.13"),
+		.package(url: "https://github.com/qizh/QizhMacroKit", exact: "1.0.14"),
 	],
     targets: [
         .target(
