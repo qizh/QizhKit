5e0b8d900895ea3be7393deb446a0cfd4b6580e3
Serhii Shevchenko <zh.send@gmail.com>
2025-02-26 01:12:14 +0700

    Add `DontEncodeEmpty` property wrapper

---
+++ Sources/QizhKit/Structures/Codable/DontEncodeEmpty.swift
@@ -0,0 +1,43 @@
+//
+//  DontEncodeEmpty.swift
+//  QizhKit
+//
+//  Created by Serhii Shevchenko on 26.02.2025.
+//  Copyright © 2024 Serhii Shevchenko. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct DontEncodeEmpty<Wrapped> where Wrapped: EmptyTestable, Wrapped: Encodable {
+	public var wrappedValue: Wrapped
+	
+	public init(wrappedValue: Wrapped) {
+		self.wrappedValue = wrappedValue
+	}
+	
+	public func encode(to encoder: Encoder) throws {
+		if wrappedValue.isNotEmpty {
+			var container = encoder.singleValueContainer()
+			try container.encode(wrappedValue)
+		}
+	}
+}
+
+extension DontEncodeEmpty: Equatable where Wrapped: Equatable { }
+extension DontEncodeEmpty: Hashable where Wrapped: Hashable { }
+extension DontEncodeEmpty: Sendable where Wrapped: Sendable { }
+
+extension DontEncodeEmpty: WithAnyDefault where Wrapped: WithDefault { }
+extension DontEncodeEmpty: WithDefault where Wrapped: WithDefault {
+	@inlinable public static var `default`: DontEncodeEmpty<Wrapped> {
+		.init(wrappedValue: Wrapped.default)
+	}
+}
+
+extension DontEncodeEmpty: WithAnyUnknown where Wrapped: WithUnknown { }
+extension DontEncodeEmpty: WithUnknown where Wrapped: WithUnknown {
+	@inlinable public static var unknown: DontEncodeEmpty<Wrapped> {
+		.init(wrappedValue: Wrapped.unknown)
+	}
+}
