e025b9ae519135dca442e7948cb77b9bf7a02364
Serhii Shevchenko <zh.send@gmail.com>
2025-03-29 00:40:14 +0700

    Add variables and method to String, converting any case of it (camelCase, snake_case, or just a regular sentence) to any other case: - [x] `toCamelCase` - [x] `toPascalCase` - [x] `toSnakeCase` - [x] `toKebabCase` - [x] `toDotCase` - [x] `toLocalizedLowercasedWords(joinedBy:)`

--- Sources/QizhKit/Extensions/String+/String+camelSnakeConvert.swift
+++ Sources/QizhKit/Extensions/String+/String+camelSnakeConvert.swift
@@ -26,10 +26,25 @@ fileprivate actor Regexes {
 			substring.first
 		}
 	}
+	
+	fileprivate static let words: Regex = Regex {
+		ChoiceOf {
+			Regex {
+				Optionally { #/\p{Lu}/# }
+				OneOrMore { #/\p{Ll}/# }
+			}
+			Regex {
+				OneOrMore { #/\p{Lu}/# }
+				NegativeLookahead { #/\p{Ll}/# }
+			}
+			OneOrMore { #/\p{L}/# }
+		}
+	}
 }
 
 extension String {
 	/// Converts a `camelCase` string to `snake_case`.
+	@available(*, deprecated, renamed: "toSnakeCase", message: "It detects words in any string, not just in camelCase")
 	public var asCamelCaseToSnakeCase: String {
 		self.replacing(Regexes.captureLargeCharactersAfterSmall) { match in
 			let firstPart = match[Regexes.beforeLargeCharacterRef]
@@ -40,6 +55,7 @@ extension String {
 	}
 	
 	/// Converts a `snake_case` string to `camelCase`.
+	@available(*, deprecated, renamed: "toCamelCase", message: "It detects words in any string, not just in snake_case")
 	public var asSnakeCaseToCamelCase: String {
 		self.split(separator: .underscore)
 			.enumerated()
@@ -51,5 +67,51 @@ extension String {
 				}
 			}
 			.joined()
+	}
+	
+	/// `toCamelCase`
+	public var toCamelCase: String {
+		self.matches(of: Regexes.words)
+			.enumerated()
+			.map { index, part in
+				if index == 0 {
+					part.localizedLowercase
+				} else {
+					part.localizedCapitalized
+				}
+			}
+			.joined()
+	}
+	
+	/// `ToPascalCase`
+	public var toPascalCase: String {
+		self.matches(of: Regexes.words)
+			.map(\.localizedCapitalized)
+			.joined()
+	}
+	
+	/// Matches words in any language and any case, lowercases them,
+	/// and joines with provided separator
+	/// - Parameter separator: String to join the words with
+	/// - Returns: Lowercased words joined with the separator
+	public func toLocalizedLowercasedWords(joinedBy separator: String) -> String {
+		self.matches(of: Regexes.words)
+			.map(\.localizedLowercase)
+			.joined(separator: separator)
+	}
+	
+	/// `to_snake_case`
+	@inlinable public var toSnakeCase: String {
+		self.toLocalizedLowercasedWords(joinedBy: .underscore)
+	}
+	
+	/// `to-kebab-case`
+	@inlinable public var toKebabCase: String {
+		self.toLocalizedLowercasedWords(joinedBy: .minus)
+	}
+	
+	/// `to.dot.case`
+	@inlinable public var toDotCase: String {
+		self.toLocalizedLowercasedWords(joinedBy: .minus)
 	}
 }
