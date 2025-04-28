a939351909bbe8c588a09b25f13e038a2328a821
Serhii Shevchenko <zh.send@gmail.com>
2025-04-28 14:20:10 +0700

    Add `CustomStringConvertible` adoption to `CLLocationCoordinate2D`

--- Sources/QizhKit/Extensions/Core Location+/CLLocationCoordinate2D+adopts.swift
+++ Sources/QizhKit/Extensions/Core Location+/CLLocationCoordinate2D+adopts.swift
@@ -9,6 +9,8 @@
 import Foundation
 import CoreLocation
 
+// MARK: Equatable
+
 extension CLLocationCoordinate2D: @retroactive Equatable {
 	public static func == (
 		lhs: CLLocationCoordinate2D,
@@ -19,9 +21,21 @@ extension CLLocationCoordinate2D: @retroactive Equatable {
 	}
 }
 
+// MARK: Hashabale
+
 extension CLLocationCoordinate2D: @retroactive Hashable {
 	public func hash(into hasher: inout Hasher) {
 		hasher.combine(latitude)
 		hasher.combine(longitude)
+	}
+}
+
+// MARK: Description
+
+extension CLLocationCoordinate2D: @retroactive CustomStringConvertible {
+	fileprivate static let format: FloatingPointFormatStyle<CLLocationDegrees> = .number.precision(.fractionLength(...6))
+	
+	public var description: String {
+		"(\(latitude.formatted(Self.format)), \(longitude.formatted(Self.format)))"
 	}
 }
