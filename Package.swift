35c16c949ce01549e86a21830db74458f7b2253b
Serhii Shevchenko <zh.send@gmail.com>
2025-09-11 01:14:24 +0700

    `QizhMacroKit` update to `1.1.6`

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
 		
 		/// Macros
-		.package(url: "https://github.com/qizh/QizhMacroKit", exact: "1.1.5"),
+		.package(url: "https://github.com/qizh/QizhMacroKit", exact: "1.1.6"),
 	],
     targets: [
         .target(
